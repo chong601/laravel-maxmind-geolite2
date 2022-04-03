596d8e6b2122cf18612e01ddc4f60b1af2f2abaa
Chong Jin Yi <chong601.13@gmail.com>
2022-04-03 22:17:28 +0800

    Rename and update migration file... again - Get rid of incrementing IDs and use the network as the primary key - Add IP minimum and maximum ranges for easy IP lookup   Still have no idea how to do it LOL - Remove default Laravel default nullable timestamps - Use automatically-created creation and updated timestamps - Fix table names to reflect Laravel best practices

---
+++ database/migrations/2022_04_02_100312_create_geoip_asns_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Carbon\Carbon;
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateGeoipAsnsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('geoip_asns', function (Blueprint $table) {
+            $table->string('network')->primary();
+            $table->bigInteger('autonomous_system_number');
+            $table->text('autonomous_system_organization');
+            $table->decimal('ip_min_range', 40, 0, true)->index()->nullable();
+            $table->decimal('ip_max_range', 40, 0, true)->index()->nullable();
+            $table->timestamp('created_at', 0)->nullable()->default(Carbon::now());
+            $table->timestamp('updated_at', 0)->nullable()->default(Carbon::now());
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('geoip_asns');
+    }
+}
---
+++ database/migrations/2022_04_02_102500_create_geoip_cities_table.php
@@ -0,0 +1,44 @@
+<?php
+
+use Carbon\Carbon;
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateGeoipCitiesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('geoip_cities', function (Blueprint $table) {
+            $table->string('network')->primary();
+            $table->integer('geoname_id');
+            $table->integer('registered_country_geoname_id')->nullable()->default(-1);
+            $table->integer('represented_country_geoname_id')->nullable()->default(-1);
+            $table->boolean('is_anonymous_proxy');
+            $table->boolean('is_satellite_provider');
+            $table->text('postal_code');
+            $table->float('latitude', 8, 4);
+            $table->float('longitude', 8, 4);
+            $table->integer('accuracy_radius');
+            $table->decimal('ip_min_range', 40, 0, true)->index()->nullable();
+            $table->decimal('ip_max_range', 40, 0, true)->index()->nullable();
+            $table->timestamp('created_at', 0)->nullable()->default(Carbon::now());
+            $table->timestamp('updated_at', 0)->nullable()->default(Carbon::now());
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('geoip_cities');
+    }
+}
