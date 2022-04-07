648268be849ed0f2b41c6d8e4e238fef52b6abd8
Chong Jin Yi <chong601.13@gmail.com>
2022-04-08 00:41:37 +0800

    Add ipType to GeoIP data

---
+++ database/migrations/2022_04_07_161538_add_ip_type_to_geoip_cities.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class AddIpTypeToGeoipCities extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('geoip_cities', function (Blueprint $table) {
+            $table->integer('ipType')->after('network');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('geoip_cities', function (Blueprint $table) {
+            $table->dropColumn('ipType');
+        });
+    }
+}
